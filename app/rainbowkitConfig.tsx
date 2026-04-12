eaf8cdd12d26e8fa815b9cd3d8c4bb90be88de3c
Dennis <kilonzod19@gmail.com>
2026-04-12 18:09:26 +0300

    Added the mainnet option

--- app/rainbowkitConfig.tsx
+++ app/rainbowkitConfig.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, zksync } from "wagmi/chains";
+import { anvil, zksync, mainnet } from "wagmi/chains";
 
 export default getDefaultConfig({
   appName: "ts-tx-sender",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  chains: [anvil, zksync],
+  chains: [anvil, zksync, mainnet],
   ssr: false,
 });
