6dab9e54375ffe1a2d8245135440ca59ef1ede6c
Dennis <kilonzod19@gmail.com>
2026-04-12 17:42:43 +0300

    Added import statements as well asQueryClientProvider, and WagmiConfig in place of WagmiProvider which is deprecated

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { ReactNode } from "react";
-import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+import { WagmiConfig } from "wagmi";
 import config from "./rainbowkitConfig";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+export function Provider(props: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
 
-export function Provider(props: {children: ReactNode}) {
-    return (
-        <WagmiProvider config={config}>
-            <RainbowKitProvider>
-                {props.children}
+  return (
+    <WagmiConfig config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+            <ConnectButton />
+            {props.children}
             </RainbowKitProvider>
-        </WagmiProvider>
-    );
+      </QueryClientProvider>
+    </WagmiConfig>
+  );
 }
