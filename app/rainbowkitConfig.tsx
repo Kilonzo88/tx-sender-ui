6c949c82c1e217789952d5744a302e34292abe46
Dennis <kilonzod19@gmail.com>
2026-04-12 17:42:49 +0300

    updated the app's name

--- app/rainbowkitConfig.tsx
+++ app/rainbowkitConfig.tsx
@@ -4,7 +4,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { anvil, zksync } from "wagmi/chains";
 
 export default getDefaultConfig({
-  appName: "RainbowKit demo",
+  appName: "ts-tx-sender",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
   chains: [anvil, zksync],
   ssr: false,
