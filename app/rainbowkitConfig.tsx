3fbd76201d213bee7d68feb479b6d8b46e0dd42f
Dennis <kilonzod19@gmail.com>
2026-04-16 19:25:13 +0300

    fix: override default Mainnet RPC transport to resolve CORS errors
    - Swapped the default  Wagmi RPC for Cloudflare's public node.
    - Resolved browser console  logs caused by RainbowKit's background ENS/avatar resolution getting blocked by CORS policies.

--- app/rainbowkitConfig.tsx
+++ app/rainbowkitConfig.tsx
@@ -2,10 +2,16 @@
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { anvil, zksync, mainnet } from "wagmi/chains";
+import { http } from "wagmi";
 
 export default getDefaultConfig({
   appName: "ts-tx-sender",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
   chains: [anvil, zksync, mainnet],
+  transports: {
+    [mainnet.id]: http("https://cloudflare-eth.com"),
+    [anvil.id]: http(),
+    [zksync.id]: http(),
+  },
   ssr: false,
 });
